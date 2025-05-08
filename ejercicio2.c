19983592882fcbe27c33949b01b41d0348b2e50f
DanyQuiroz2 <dqc220618@gmail.com>
2025-05-08 14:25:19 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main()
+{
+  int list [5];
+  int1, calif=0;
+  float prom;
+
+  for(1=0;1<5; 1++
+  
+  {
+    printf("introducir calificaciiob numero%d: *,i+1);
+    scanf("%d, &list[i]);
+  }
+
+  for(1=0; 1<5; i++)
+  {
+    calif=calif+ list (i);
+  }
+
+
+    prom= calif/5;
+
+    printf("el promedio de las calificaciones es:%f\n", prom);
+}
+  
+  
