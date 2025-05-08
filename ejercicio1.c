ff82111ec48f2a3fcf01c7f1f83b713d66906de9
DanyQuiroz2 <dqc220618@gmail.com>
2025-05-08 14:09:42 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+  int list [5]={57,73,24,12,2o};
+  int 1;
+  printf("los numeros de la lista son;")
+  for (i=0; i<5; 1++)
+  {
+   printf/"%d,", list [1]);
+  }
+  printif("\n");
+}
+ 
